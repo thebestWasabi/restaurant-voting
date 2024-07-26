016c21272297535eee3321b000f2e68cc99cb853
wasabi <openpw@icloud.com>
2024-07-26 22:51:01 +0300

    add: 4_05_role_auth

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO USERS (EMAIL, FIRST_NAME, LAST_NAME, PASSWORD)
 VALUES ('user@gmail.com', 'User_First', 'User_Last', '{noop}password'),
        ('admin@javaops.ru', 'Admin_First', 'Admin_Last', '{noop}admin');
 INSERT INTO USER_ROLE (ROLE, USER_ID)
-VALUES ('ROLE_USER', 1),
-       ('ROLE_ADMIN', 2),
-       ('ROLE_USER', 2);
+VALUES ('USER', 1),
+       ('ADMIN', 2),
+       ('USER', 2);
