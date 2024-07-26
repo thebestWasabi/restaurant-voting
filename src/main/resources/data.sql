b7b9673686f9de7e9b0d613fd9268a1b8447212c
wasabi <openpw@icloud.com>
2024-07-26 21:43:37 +0300

    add: 4_01_add_security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,5 @@ VALUES ('user@gmail.com', 'User_First', 'User_Last', 'password'),
 INSERT INTO USER_ROLE (ROLE, USER_ID)
 VALUES ('ROLE_USER', 1),
        ('ROLE_ADMIN', 2),
-       ('ROLE_USER', 2);
+       ('ROLE_USER', 2),
+       ('ROLE_USER', 3);
